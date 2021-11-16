199bd9b8b6a5d1397331ad9ee5c95eea0c0a7ce7
Jayesh <jayesh.bhosale1812@gmail.com>
2021-11-16 19:00:32 +0530

    Contact created

---
+++ Adressbook.java
@@ -0,0 +1,9 @@
+package bridgelabz.Adressbook;
+
+public class Adressbook {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to Address book Program");
+	        Utility utility=new Utility();
+	        utility.contactDetails();
+	    }
+}
---
+++ Welcome.java
@@ -0,0 +1,8 @@
+package bridgelabz.Adressbook;
+
+public class Welcome {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to address book");
+}
+}
